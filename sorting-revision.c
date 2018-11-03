e42b8802bcacb6d86b0dcb11b0beada7190e4628
Assaad Oussama Zeghina <las9ayne_215@hotmail.com>
2018-11-03 16:29:47 +0100

    adding sorting algorithms in c(revision)

---
+++ sorting-revision.c
@@ -0,0 +1,50 @@
+#include <stdio.h> 
+#include <math.h> 
+  
+void insertionSort(int arr[], int n) 
+{ 
+   int i, key, j; 
+   for (i = 1; i < n; i++) 
+   { 
+       key = arr[i]; 
+       j = i-1; 
+
+       while (j >= 0 && arr[j] > key) 
+       { 
+           arr[j+1] = arr[j]; 
+           j = j-1; 
+       } 
+       arr[j+1] = key; 
+   } 
+} 
+
+//swap func for the selection and bubble sort codes
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min_idx; 
+ 
+    for (i = 0; i < n-1; i++) 
+    {       
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+   
+        swap(&arr[min_idx], &arr[i]); 
+    } 
+} 
+
+void bubbleSort(int arr[], int n) 
+{ 
+   int i, j; 
+   for (i = 0; i < n-1; i++)          
+       for (j = 0; j < n-i-1; j++)  
+           if (arr[j] > arr[j+1]) 
+              swap(&arr[j], &arr[j+1]); 
+}
